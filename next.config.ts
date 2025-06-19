7c59bdc38e6cf7d40108896b517f71dd0c77cb8e
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-19 13:11:39 -0300

    feat: configs and libs imports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                hostname: "cdn1.iconfinder.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
