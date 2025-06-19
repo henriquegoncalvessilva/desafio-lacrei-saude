420fbd06007b6375c84012706782b537bad50eda
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-19 13:10:37 -0300

    Feat: setup of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,87 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import "material-symbols";
+import dynamic from "next/dynamic";
+const Card = dynamic(() => import("./components/ui/card/card"), { ssr: false });
+import { Divider } from "./components/ui/divider";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Professional } from "@/interfaces/professional.interface";
+const Title = dynamic(
+    () => import("./components/ui/title/title").then((mod) => mod.Title),
+    { ssr: false }
+);
+const TitleContainer = dynamic(
+    () =>
+        import("./components/ui/title/title").then((mod) => mod.TitleContainer),
+    { ssr: false }
+);
+const TitleSM = dynamic(
+    () => import("./components/ui/title/style").then((mod) => mod.TitleSM),
+    { ssr: false }
+);
+const CardContentContainer = dynamic(
+    () => import("./components/ui/containers/cardContentContainer"),
+    {
+        ssr: false,
+        loading: () => (
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "100%",
+                    margin: "0 auto",
+                    textAlign: "center",
+                    padding: "20px",
+                    height: "100%",
+                }}
+            >
+                <h1>Carregando...</h1>
+            </div>
+        ),
+    }
+);
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [questions, setQuestions] = React.useState<Professional[] | null>(
+        null
+    );
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    useEffect(() => {
+        axios
+            .get<Professional[]>("http://localhost:3000/professionals")
+            .then((response) => {
+                setQuestions(response.data);
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    useEffect(() => {
+        console.log(questions);
+    }, [questions]);
+
+    return (
+        <>
+            <TitleContainer>
+                <Title
+                    title="Lista de Cards"
+                    style={{ textAlign: "center" }}
+                    type="XL"
+                />
+                <Divider />
+                <TitleSM>
+                    Sinta-se à vontade para escolher o profissional que melhor
+                    atende às suas necessidades, de forma segura e livre de
+                    preconceitos.
+                </TitleSM>
+            </TitleContainer>
+            <CardContentContainer>
+                {questions?.map((professional, index) => {
+                    return <Card professinal={professional} key={index} />;
+                })}
+            </CardContentContainer>
+        </>
+    );
 }
