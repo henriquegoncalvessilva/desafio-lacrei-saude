0abd4e61762b640e862fc6de1f170a402738007c
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-19 13:08:38 -0300

    feat: added interface to professionals

---
+++ src/interfaces/professional.interface.tsx
@@ -0,0 +1,27 @@
+type Race = "negra" | "branca" | "parda" | "indígena" | "amarela";
+
+type Orientation =
+    | "lesbica"
+    | "gay"
+    | "bissexual"
+    | "assexual"
+    | "pansexual"
+    | "heterossexual";
+
+interface Tag {
+    label: Race;
+    icon: Orientation;
+}
+
+export interface Professional {
+    name: string;
+    area: string;
+    register: string;
+    tags: Tag[];
+    location: string;
+    schedule: string;
+    duration: string;
+    adress: string;
+    cost: string;
+    payment: string;
+}
