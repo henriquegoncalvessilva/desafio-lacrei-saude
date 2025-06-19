311ea265a94036736ed5edc13e3107264a4650f7
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-19 19:04:49 -0300

    feat: Added mock in help page too.  Ps: Keep json server and also it works. But in this moment I prefer mock data.

--- src/app/ajuda/page.tsx
+++ src/app/ajuda/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { NextPage } from "next";
-import React, { useEffect } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
-import axios from "axios";
 const BreadCrumb = dynamic(
     () => import("../components/ui/breadcrumb/breadcrumb")
 );
@@ -19,22 +18,29 @@ export interface Question {
     answer: string;
 }
 const Ajuda: NextPage = () => {
-    const [questions, setQuestions] = React.useState<Question[] | null>();
-
-    useEffect(() => {
-        axios
-            .get<Question[]>("http://localhost:3000/help")
-            .then((response) => {
-                setQuestions(response.data);
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-    useEffect(() => {
-        console.log(questions);
-    }, [questions]);
+    const mock: Question[] = [
+        // {
+        //     question: "Os Profissionais são da Lacrei Saúde?",
+        //     answer: "Não. Os profissionais são autonomos e não possuem vinculo com a Lacrei Saúde.",
+        // },
+        // {
+        //     question:
+        //         "Os Prossifionais estão aptos a atenderem pessoas com deficiencia? Por exemplo pessoas surdas?",
+        //     answer: "Sim, alguns dos profissionais sabem LIBRAS e conseguem atender surdos sem nenhuma dificuldade.",
+        // },
+        // {
+        //     question: "Os atendimentos são humanizados?",
+        //     answer: "Sim, todos os atendimentos são humanizados. Todos os profissionais que atendem através da Lacrei saúde, passam por entrevistas antes de iniciar seus atendimentos.",
+        // },
+        // {
+        //     question: "A Lacrei Saúde aceita todas as formas de pagamento?",
+        //     answer: "Sim! Aceitamos todas as formas de pagamento, como: PIX, Cartão de Crédito e Boleto Bancário.",
+        // },
+        // {
+        //     question: "A Lacrei Saúde atua em quais áreas médicas?",
+        //     answer: "Possuimos a parceria com diversos profissionais de saúde, que por sua vez, atuam em diversas especialidades.",
+        // },
+    ];
 
     return (
         <>
@@ -46,15 +52,31 @@ const Ajuda: NextPage = () => {
                     <p>Aqui, você encontra as dúvidas mais frequentes!</p>
                 </TitleContainerDuvidas>
 
-                {questions?.map((question, index) => {
-                    return (
-                        <Accordion
-                            key={index}
-                            labelAnswer={question.answer}
-                            labelQuestion={question.question}
-                        />
-                    );
-                })}
+                {mock.length > 0 ? (
+                    mock?.map((question, index) => {
+                        return (
+                            <Accordion
+                                key={index}
+                                labelAnswer={question.answer}
+                                labelQuestion={question.question}
+                            />
+                        );
+                    })
+                ) : (
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            height: "100%",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <h4>
+                            Algo de errado aconteceu. Tente novamente mais
+                            tarde.
+                        </h4>
+                    </div>
+                )}
             </ContainerDuvidasFrequentes>
         </>
     );
