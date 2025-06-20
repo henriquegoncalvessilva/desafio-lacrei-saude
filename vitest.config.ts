a96075666abebee6858b0aecbfc4361c82f86005
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-06-20 16:00:53 -0300

    feat: Report unit test

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        environment: "jsdom", // ambiente para testes React
-        globals: true, // para usar globals tipo `describe`, `it`, `expect`
-        setupFiles: "./vitest.setup.ts", // arquivo para configurar jest-dom etc
+        reporters: ["default", "json"],
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./vitest.setup.ts",
+        outputFile: "vitest-report.json",
     },
 });
